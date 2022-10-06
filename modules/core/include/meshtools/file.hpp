72aa522b07969cfba006519defeaea5f32f5d108
Ivo van Dongen <info@ivovandongen.nl>
2022-10-06 17:26:29 +0300

    [core] add write file function

--- modules/core/include/meshtools/file.hpp
+++ modules/core/include/meshtools/file.hpp
@@ -1,11 +1,30 @@
 #pragma once
 
 #include <filesystem>
+#include <fstream>
+#include <iostream>
 
 namespace meshtools::file {
 
 static inline bool parentDirExists(const std::filesystem::path& path) {
     return !path.empty() && std::filesystem::exists(path.parent_path());
+}
+
+template<class T>
+inline void writeFile(const std::string& fileName, const T& data, bool binary = false) {
+    // TMP
+    auto flags = std::ios::out;
+    if (binary) {
+        flags |= std::ios::binary;
+    }
+    std::ofstream file(fileName, flags);
+    if (file.is_open()) {
+        file.write(data.data(), data.size());
+        file.close();
+    } else {
+        std::cerr << "Cannot write to file: " << fileName << std::endl;
+        abort();
+    }
 }
 
 } // namespace meshtools::file
